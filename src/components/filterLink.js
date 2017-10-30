55039f6736211ebfb005babd1179b39614f5bf98
Von Haynes <drbrounsuga@gmail.com>
2017-10-29 23:42:21 -0500

    replaced default stylesheet

--- src/components/filterLink.js
+++ src/components/filterLink.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import '../assets/styles/App.css';
-
 const FilterLink = ({ filter, children }) => (
   <Link
     to={filter === 'SHOW_ALL' ? '/' : `/${ filter }`}
