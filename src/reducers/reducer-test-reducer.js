57339defecb63e2bfe7bd2ee8c2f1c89cb15bd7e
Von Haynes <drbrounsuga@gmail.com>
2017-10-29 21:38:24 -0500

    replaced object.assign with spread operator

--- src/reducers/reducer-test-reducer.js
+++ src/reducers/reducer-test-reducer.js
@@ -1,14 +1,22 @@
 import T from '../actions/types';
 
-const initialState = { test: 0 };
+const initialState = { 
+  test: 0 
+};
 
 export default function(state = initialState, action){
   switch(action.type){
     case T.TEST_INCREMENT:
-      return Object.assign({}, state, {test: state.test + action.payload});
+      return {
+        ...state,
+        test: state.test + action.payload
+      };
 
     case T.TEST_DECREMENT:
-      return Object.assign({}, state, {test: state.test - action.payload});
+      return {
+        ...state,
+        test: state.test - action.payload
+      };
 
     default:
       return state;
